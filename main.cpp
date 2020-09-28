62034d38e8e0af4f2311151e1db22f2a0e60d2ca
Jaykant Tiano <jtiano19@go.byuh.edu>
2020-09-28 23:28:50 +0000

    Create instances of dispensers and register

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ int main(int argc, char **argv){
     int totalItems = 6;
 
    
+    Register reg(registerCash);
+    Dispenser healingPotion("Healing Potion", 3, 50);
+    Dispenser manaPotion("Mana Potion", 5, 40);
+    Dispenser fullRestore("Full Restore", 20, 30);
+    Dispenser revivePotion("Revive Potion", 50, 10);
+    Dispenser elixir("Elixir", 30, 20);
+    Dispenser antidote("Antidote", 10, 35);
+    Dispenser stocks[totalItems] = {healingPotion, manaPotion, fullRestore, revivePotion, elixir, antidote};
     cout << "Welcome Adventurer!" << endl << "How may I serve your today?";
 
 
