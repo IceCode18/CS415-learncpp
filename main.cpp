8002bfb2c133d36b3878c86f8649c751b95f77e3
Jaykant Tiano <jtiano19@go.byuh.edu>
2020-09-28 22:54:29 +0000

    Add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv){
+    double registerCash = 50;
+    int totalItems = 6;
+
+   
+    cout << "Welcome Adventurer!" << endl << "How may I serve your today?";
+
+
+}
